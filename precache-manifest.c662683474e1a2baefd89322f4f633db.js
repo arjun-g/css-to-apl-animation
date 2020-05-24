ee6895475d6d6ba1288e4202c79ec63f3c755007
Arjun Ganesan <me@arjun-g.com>
2020-05-24 20:40:23 +0530

    Updates

---
+++ precache-manifest.c662683474e1a2baefd89322f4f633db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bd9abcb10288e10762eb40ff23ecd6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ccdbfb91f8b1669cf044",
+    "url": "/static/css/main.f54afe54.chunk.css"
+  },
+  {
+    "revision": "389a358551ce759bf6dc",
+    "url": "/static/js/2.e1589152.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e1589152.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccdbfb91f8b1669cf044",
+    "url": "/static/js/main.2c0eac7b.chunk.js"
+  },
+  {
+    "revision": "fa0af807e30880911a9d",
+    "url": "/static/js/runtime-main.28faca3d.js"
+  }
+]);
